7a22102d3ee83a107325ad54960760d69ff67cf1
Aleksey Belchenkov <u608110@gmail.com>
2020-11-16 13:32:30 +0300

    REST api for Users

--- laravel-admin/app/Http/Controllers/UserController.php
+++ laravel-admin/app/Http/Controllers/UserController.php
@@ -4,11 +4,53 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Hash;
+use function Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function index()
+    public function index() : Collection
     {
         return User::all();
+    }
+
+    public function show(int $id) : User
+    {
+        return User::find($id);
+    }
+
+    public function store(Request $request) : Response
+    {
+        $user = User::create([
+            'first_name' => $request->input('first_name'),
+            'last_name' => $request->input('last_name'),
+            'email' => $request->input('email'),
+            'password' => Hash::make($request->input('password'))
+        ]);
+
+        return response($user, Response::HTTP_CREATED);
+    }
+
+    public function update(Request $request, int $id) : Response
+    {
+        $user = User::find($id);
+
+        $user->update([
+            'first_name' => $request->input('first_name'),
+            'last_name' => $request->input('last_name'),
+            'email' => $request->input('email'),
+            'password' => Hash::make($request->input('password'))
+        ]);
+
+        return response($user, Response::HTTP_ACCEPTED);
+    }
+
+    public function destroy(int $id) : Response
+    {
+        User::destroy($id);
+
+        return response(null, Response::HTTP_NO_CONTENT);
     }
 }
--- laravel-admin/routes/api.php
+++ laravel-admin/routes/api.php
@@ -15,4 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/users', [UserController::class, 'index']);
+Route::apiResource('users', UserController::class);
