cd8bae2e98575c3e1829d5473f3cf64758b33345
Aleksey Belchenkov <u608110@gmail.com>
2021-01-14 15:46:16 +0300

    Foreigh Keys Roles

--- laravel-admin/.dbdata/sys/x@0024session.frm
+++ laravel-admin/.dbdata/sys/x@0024session.frm
@@ -7,7 +7,7 @@ definer_user=mysql.sys
 definer_host=localhost
 suid=0
 with_check_option=0
-timestamp=2020-11-11 16:11:34
+timestamp=2021-01-13 12:13:49
 create-version=1
 source=SELECT * FROM sys.x$processlist WHERE conn_id IS NOT NULL AND command != \'Daemon\'
 client_cs_name=utf8
--- laravel-admin/database/factories/UserFactory.php
+++ laravel-admin/database/factories/UserFactory.php
@@ -2,9 +2,9 @@
 
 namespace Database\Factories;
 
+use App\Models\Role;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Str;
 
 class UserFactory extends Factory
 {
@@ -26,6 +26,7 @@ class UserFactory extends Factory
             'first_name' => $this->faker->firstName,
             'last_name' => $this->faker->lastName,
             'email' => $this->faker->unique()->safeEmail,
+            'role_id' => random_int(1, 3),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         ];
     }
